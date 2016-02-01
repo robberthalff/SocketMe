8abe6c12e1811fa05313781bcbf5140ccebba542
Rob Halff <rob.halff@gmail.com>
2016-02-01 19:25:05 +0100

    clean

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('SocketMe', [
   'ionic',
   'ngCordova',
@@ -28,14 +21,8 @@ angular.module('SocketMe', [
   })
 
   .config(function ($stateProvider, $urlRouterProvider, $logProvider) {
-
     $logProvider.debugEnabled(false);
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
     $stateProvider
-
       .state('tab', {
         url: '/tab',
         abstract: true,
@@ -74,5 +61,4 @@ angular.module('SocketMe', [
 
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/tab/dash')
-
   })
